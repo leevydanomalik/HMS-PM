a4c000787be045af569d961e00cf41f9692cf0dd
dumayangsari <dumayangsari09@gmail.com>
2021-10-15 19:13:08 +0700

    upload/download doc movement

--- hms.pm.command.svc/src/main/java/com/bitozen/hms/web/controller/movement/MovementDocumentController.java
+++ hms.pm.command.svc/src/main/java/com/bitozen/hms/web/controller/movement/MovementDocumentController.java
@@ -1,6 +1,13 @@
 package com.bitozen.hms.web.controller.movement;
 
+import com.bitozen.hms.common.dto.GenericResponseDTO;
+import com.bitozen.hms.common.type.ProjectType;
+import com.bitozen.hms.common.util.LogOpsUtil;
+import com.bitozen.hms.web.controller.movement.MovementDocumentController;
+import com.bitozen.hms.web.hystrix.movement.MovementDocumentHystrixService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Date;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -8,6 +15,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @CrossOrigin(methods = {RequestMethod.GET, RequestMethod.DELETE})
@@ -22,5 +37,47 @@ public class MovementDocumentController {
     @Autowired
     private HttpServletRequest httpRequest;
 
+    @Autowired
+    private MovementDocumentHystrixService service;
 
+    @PostMapping("/post.movement.document")
+    @CrossOrigin(origins = "*", methods = {RequestMethod.POST, RequestMethod.OPTIONS},
+            allowedHeaders = {"Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"},
+            exposedHeaders = {"Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"})
+    public ResponseEntity<GenericResponseDTO<String>> uploadMovementDocumentFile(@RequestParam("file") MultipartFile upload, @RequestParam("mvID") String mvID, @RequestParam("docID") String docID) {
+        try {
+            log.info(objectMapper.writeValueAsString(
+                    LogOpsUtil.getLogOps(ProjectType.CQRS, "Movement Document", MovementDocumentController.class.getName(),
+                            httpRequest.getRequestURL().toString(),
+                            new Date(), "Command", "Upload",
+                            "SYSTEM",
+                            mvID)));
+        } catch (JsonProcessingException ex) {
+            log.info(ex.getMessage());
+        }
+        if (upload.isEmpty()) {
+            return new ResponseEntity("Please select a file.", HttpStatus.OK);
+        }
+
+        GenericResponseDTO<String> response = service.saveUploadedFile(upload, mvID, docID);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @RequestMapping(value = "/get.movement.document/{mvID}/{docID}", method = RequestMethod.GET, produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<byte[]> getMovementDocumentURL(@PathVariable("mvID") String mvID, @PathVariable("docID") String docID) {
+        GenericResponseDTO<byte[]> response = new GenericResponseDTO().successResponse();
+        try {
+            log.info(objectMapper.writeValueAsString(
+                    LogOpsUtil.getLogOps(ProjectType.CQRS, "Movement Document", MovementDocumentController.class.getName(),
+                            httpRequest.getRequestURL().toString(),
+                            new Date(), "Command", "getOrderDocumentUrl",
+                            "SYSTEM",
+                            mvID)));
+            response = service.generateMovementDocumentURLByte(mvID, docID);
+            return ResponseEntity.status(HttpStatus.OK).body(response.getData());
+        } catch (Exception ex) {
+            log.info(ex.getMessage());
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(response.getData());
+    }
 }
